e35555edc191a111d8ee724425a8945ab49b0044
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2022-04-26 09:23:22 -0500

    sin cambios más que dividir responsabilidad

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 // Part 1 Read json file ===========================
 const rawdata = fs.readFileSync("explorers.json");
-const explorers = JSON.parse(rawdata);
+const explorers = JSON.parse(rawdata);// analiza el json como texto
 // Part 2: Get the quantity of explorers names in node
 const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
 //console.log(explorersInNode.length)

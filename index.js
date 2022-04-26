041c4a92d764edd86badd3cf44bde66e233addc7
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2022-04-26 11:28:00 -0500

    ExplorerServices en el index

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const Reader = require('./lib/utils/Reader');
+const ExplorerServices = require('./lib/services/ExplorerService')
 
 const explorers = Reader.readJsonFile("explorers.json");
-console.log(explorers);
+//console.log(explorers);
+
+console.log(ExplorerServices.filterByMission(explorers,"node"));

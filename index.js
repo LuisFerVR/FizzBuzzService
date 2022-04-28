7f04ac36652e9951bd31e02cfe2c82a3e567620e
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2022-04-28 16:26:24 -0500

    eliminando par de archivos demás como index antes de usar pruebas al comienzo y app.js que fué el anterior proyecto que estaba

--- index.js
+++
@@ -1,7 +0,0 @@
-const Reader = require('./lib/utils/Reader');
-const ExplorerServices = require('./lib/services/ExplorerService')
-
-const explorers = Reader.readJsonFile("explorers.json");
-//console.log(explorers);
-
-console.log(ExplorerServices.filterByMission(explorers,"node"));

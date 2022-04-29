453b5a60f88fe7d43acd0b12c9d02672cd522eea
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2022-04-29 13:56:56 -0500

    prueba montada y pasada con la lógica de la clase ExplorerController

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,7 +1,7 @@
-const ExploreController = require('../../lib/controllers/ExplorerController');
-
+const ExploreController = require("../../lib/controllers/ExplorerController");
 describe("test para clase ExplorerController", () => {
     test("metodo 1", () => {
-        
-    })
-})
+        const filtrado = ExploreController.getExplorersByMission("node");
+        expect(filtrado.length).toBe(10);
+    });
+});

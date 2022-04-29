27ba9a8b5d76af544cbe8d3208eb7301d2b80d63
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2022-04-29 12:13:45 -0500

    directorio test para ExplorerController y cascaron de prueba metodo 1

---
+++ test/controllers/ExplorerController.test.js
@@ -0,0 +1,7 @@
+const ExploreController = require('../../lib/controllers/ExplorerController');
+
+describe("test para clase ExplorerController", () => {
+    test("metodo 1", () => {
+        
+    })
+})

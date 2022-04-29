47551f8a889ec8504ad64c3ba4d5e773954a03c0
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2022-04-28 21:49:51 -0500

    correr eslinter y limpiar codigo con npm run linter y npm run linter-fix

--- test/services/FizzBuzzService.test.js
+++ test/services/FizzBuzzService.test.js
@@ -1,24 +1,24 @@
-const FizzBuzzService = require('../../lib/services/FizzBuzzService');
+const FizzBuzzService = require("../../lib/services/FizzBuzzService");
 
 describe("test para FizzBuzzService", () => {
     test("test 1", () => {
-        const explorer1 = {name: "Explorer1", score:1}
-        const res1 = FizzBuzzService.applyValidationInExplorer(explorer1) // {name: "Explorer1", score: 1, trick: 1} 
+        const explorer1 = {name: "Explorer1", score:1};
+        const res1 = FizzBuzzService.applyValidationInExplorer(explorer1); // {name: "Explorer1", score: 1, trick: 1} 
         expect(res1).toStrictEqual({name: "Explorer1", score: 1, trick: 1});
     }),
-    test('test 2', () =>{
-        const explorer3 = {name: "Explorer3", score: 3}
-        const res2 = FizzBuzzService.applyValidationInExplorer(explorer3) // {name: "Explorer3", score: 3, trick: "FIZZ"}
-        expect(res2).toStrictEqual({name: "Explorer3", score: 3, trick: "FIZZ"})
+    test("test 2", () =>{
+        const explorer3 = {name: "Explorer3", score: 3};
+        const res2 = FizzBuzzService.applyValidationInExplorer(explorer3); // {name: "Explorer3", score: 3, trick: "FIZZ"}
+        expect(res2).toStrictEqual({name: "Explorer3", score: 3, trick: "FIZZ"});
     }),
-    test('test 3', () =>{
-        const explorer5 = {name: "Explorer5", score: 5}
-        const res3 = FizzBuzzService.applyValidationInExplorer(explorer5) // {name: "Explorer5", score: 5, trick: "BUZZ"}
-        expect(res3).toStrictEqual({name: "Explorer5", score: 5, trick: "BUZZ"})
+    test("test 3", () =>{
+        const explorer5 = {name: "Explorer5", score: 5};
+        const res3 = FizzBuzzService.applyValidationInExplorer(explorer5); // {name: "Explorer5", score: 5, trick: "BUZZ"}
+        expect(res3).toStrictEqual({name: "Explorer5", score: 5, trick: "BUZZ"});
     }),
-    test('test 4', () =>{
-        const explorer15 = {name: "Explorer15", score: 15}
-        const res4 = FizzBuzzService.applyValidationInExplorer(explorer15) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
-        expect(res4).toStrictEqual({name: "Explorer15", score: 15, trick: "FIZZBUZZ"})
-    })
-})
+    test("test 4", () =>{
+        const explorer15 = {name: "Explorer15", score: 15};
+        const res4 = FizzBuzzService.applyValidationInExplorer(explorer15); // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
+        expect(res4).toStrictEqual({name: "Explorer15", score: 15, trick: "FIZZBUZZ"});
+    });
+});
--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,13 +1,13 @@
-const  reader = require('../../lib/utils/Reader');//seeguro
-describe('test para Reader',() => {
-  test('1) recibir lista de explorers', () => {
-    const explorers = reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
-    //expect(reader.readJsonFile("explorers.json")).toStrictEqual(explorers);
-    expect(explorers.length).toBe(15);
-  })
+const  reader = require("../../lib/utils/Reader");//seeguro
+describe("test para Reader",() => {
+    test("1) recibir lista de explorers", () => {
+        const explorers = reader.readJsonFile("explorers.json"); // esto regresa la lista de explorers del archivo
+        //expect(reader.readJsonFile("explorers.json")).toStrictEqual(explorers);
+        expect(explorers.length).toBe(15);
+    });
 });
 
-  //TDD Test Driven Develpment
+//TDD Test Driven Develpment
 //1) escribir una prueba de lo que quieres modelar
 //2) correr la prueba vacia y que truene
 //3) escribir el código para hacer que la prueba pase

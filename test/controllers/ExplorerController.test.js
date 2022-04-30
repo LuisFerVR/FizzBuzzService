fcc906fc3bcadccc3ce742096a9ce57bfb3cb7e9
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2022-04-29 19:06:02 -0500

    prueba del metodo 3 para obtener cantidad de explorers

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -7,5 +7,9 @@ describe("test para clase ExplorerController", () => {
     test("metodo 2", () => {
         const listUserNamesFilterr = ExploreController.getExplorersUsernamesByMission("node");
         expect(listUserNamesFilterr.length).toBe(10);
+    }),
+    test("metodo 3", () => {
+        const listCantExplorers = ExploreController.getExplorersAmonutByMission("node");
+        expect(listCantExplorers).toBe(10);
     });
 });
